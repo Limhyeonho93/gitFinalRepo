ff733a86edbaf3001e16bbcd87adcf0dbd16beac
DESKTOP-SJC5QT1\user1 <25chestnut@gmail.com>
2025-05-12 16:24:24 +0900

    화물 조회 수정

--- src/main/java/kr/or/iei/common/JDBCTemplate.java
+++ src/main/java/kr/or/iei/common/JDBCTemplate.java
@@ -9,12 +9,13 @@ import java.sql.SQLException;
 public class JDBCTemplate {
 	
 	public static Connection getConnection() {
-		Connection conn = null;
-
+	Connection conn=null;
+		
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe", "server_notice", "1234");
-			conn.setAutoCommit(false);
+			conn=DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe","semi_project","1234");
+			conn.setAutoCommit(false);//자동 커밋 방지. 트랜잭션 관리는 서비스 계층에서 처리
+		
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -22,6 +23,7 @@ public class JDBCTemplate {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		return conn;
 	}
 
