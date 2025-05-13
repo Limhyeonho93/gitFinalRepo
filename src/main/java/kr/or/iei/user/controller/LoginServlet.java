750792ce7bc322f8b68944433ab2d8cacbdce81c
rlarlejr88 <kimkdluk@gmail.com>
2025-05-13 18:09:48 +0900

    index.jsp 수정, join 전체 수정, login 추가 및 수정. header.jsp 수정 resources 추가 및 css 레이아웃 추가

---
+++ src/main/java/kr/or/iei/user/controller/LoginServlet.java
@@ -0,0 +1,73 @@
+package kr.or.iei.user.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.iei.user.model.service.UserService;
+import kr.or.iei.user.model.vo.User;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/user/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 인코딩 - 필터
+		// 2. 값 추출
+		String userId = request.getParameter("userId");
+		String userPw = request.getParameter("userPw");
+		
+		// 3. 로직 - 입력한 정보와 DB에 있는 정보 비교
+		UserService service = new UserService();
+		User loginUser = service.loginUser(userId, userPw);
+		
+		// 4. 결과처리
+		RequestDispatcher view = null;
+		
+		if(loginUser == null) { // 로그인 실패
+			// 로그인 실패 시 이동할 페이지는 여기로
+			view = request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp");
+			
+			// 화면에 보여줄거
+			request.setAttribute("title", "???");
+			request.setAttribute("msg", "아이디 또는 비밀번호가 일치하지 않습니다.");
+			request.setAttribute("icon", "error.png");
+			request.setAttribute("loc", "/user/loginForm");
+		}else { // Login successful
+            request.getSession().setAttribute("loginUser", loginUser);
+            response.sendRedirect(request.getContextPath() + "/index.jsp");
+            return;
+		}
+        view.forward(request, response);
+
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/main/java/kr/or/iei/user/model/dao/UserDao.java
+++ src/main/java/kr/or/iei/user/model/dao/UserDao.java
@@ -2,6 +2,7 @@ package kr.or.iei.user.model.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import kr.or.iei.common.JDBCTemplate;
@@ -11,33 +12,70 @@ public class UserDao {
 
 	public int insertUser(Connection conn, User user) {
 		PreparedStatement pstmt = null;
-		
+
 		int result = 0;
-		
+
 		String query = "INSERT INTO T_USERS (user_id, comp_cd, user_pw, user_name, dept_name, tel_no, reg_date, upd_date ) VALUES (?, ?, ?, ?, ?, ?, sysdate, sysdate)";
-		
-		
+
 		try {
 			pstmt = conn.prepareStatement(query);
-			
+
 			pstmt.setString(1, user.getUserId());
 			pstmt.setString(2, user.getCompCd());
 			pstmt.setString(3, user.getUserPw());
 			pstmt.setString(4, user.getUserName());
 			pstmt.setString(5, user.getDeptName());
 			pstmt.setString(6, user.getTelNo());
-			
+
 			result = pstmt.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally {
+		} finally {
 			JDBCTemplate.close(pstmt);
 		}
-				
+
 		return result;
 	}
-	
-	
+
+	public User loginUser(Connection conn, String userId, String userPw) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+
+		User loginUser = null;
+
+		String query = "SELECT * FROM T_USERS WHERE user_id = ? AND user_pw = ?";
+
+		try {
+			pstmt = conn.prepareStatement(query);
+
+			pstmt.setString(1, userId);
+			pstmt.setString(2, userPw);
+
+			rset = pstmt.executeQuery();
+
+			if (rset.next()) {
+				loginUser = new User();
+
+				loginUser.setUserId(rset.getString("user_id"));
+				loginUser.setCompCd(rset.getString("comp_cd"));
+				loginUser.setUserPw(rset.getString("user_pw"));
+				loginUser.setUserName(rset.getString("user_name"));
+				loginUser.setDeptName(rset.getString("dept_name"));
+				loginUser.setTelNo(rset.getString("tel_no"));
+				loginUser.setRegDate(rset.getDate("reg_date"));
+				loginUser.setUpdDate(rset.getDate("upd_date"));
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(rset);
+			JDBCTemplate.close(pstmt);
+		}
+
+		return loginUser;
+	}
 }
