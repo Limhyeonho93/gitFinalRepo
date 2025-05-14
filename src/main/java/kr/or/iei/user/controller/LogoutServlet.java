9f905902d5a14d54b349b60ef7e8ac227cfd08fa
rlarlejr88 <kimkdluk@gmail.com>
2025-05-14 16:45:15 +0900

    관리자 계정 추가 (admin,12) 아이디,비번 // 로그아웃 추가

---
+++ src/main/java/kr/or/iei/user/controller/LogoutServlet.java
@@ -0,0 +1,49 @@
+package kr.or.iei.user.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/user/logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 현재 세션을 가져오기
+		HttpSession session = request.getSession(false); // 만약 현제 세션이 없으면 false 로 null 리턴 
+		
+		if(session != null) {
+			session.invalidate(); // 세션 무효화
+		}
+		
+		// 로그아웃 후 메인 페이지로 이동
+		response.sendRedirect(request.getContextPath() + "/");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
